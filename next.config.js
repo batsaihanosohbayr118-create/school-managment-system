3414fb4bed5826dd0fe34ebdadbaa4b7c5655e89
batsaihanosohbayr118-create <batsaihanosohbayr118@gmail.com>
2026-06-29 15:56:22 +0800

    add offline fallback

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const withPWA = withPWAInit({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  buildExcludes: [/middleware-manifest\.json$/]
+  buildExcludes: [/middleware-manifest\.json$/],
+  fallbacks: {
+    document: "/offline.html",
+  },
 });
 
 export default withPWA({
